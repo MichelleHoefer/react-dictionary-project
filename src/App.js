9ce6bba8dfea45108ddb56f41d02159da5f28fc7
Michelle Hoefer <michellehoefer7@gmail.com>
2025-06-29 14:23:57 +0100

    search bar created and event listeners created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from "react";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="app-header">React Dictionary App</header>
+      <div className="container">
+        <header className="app-header">React Dictionary App</header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="app-footer">Coded by..... etc</footer>
+      </div>
     </div>
   );
 }
