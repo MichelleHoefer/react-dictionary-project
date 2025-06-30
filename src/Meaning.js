9801e02e559dba01dea4d6373e2da737e68b05ca
Michelle Hoefer <michellehoefer7@gmail.com>
2025-06-30 10:49:41 +0100

    added synonyms

--- src/Meaning.js
+++ src/Meaning.js
@@ -2,11 +2,28 @@ import React from "react";
 import "./Meaning.css";
 
 export default function Meaning(props) {
+  const meaning = props.meaning;
+
+  if (!meaning) return null;
+
   return (
-    <div>
-      <h4>{props.meaning.partOfSpeech}</h4>
-      <p>{props.meaning.definition}</p>
-      {props.meaning.example && <em>Example: {props.meaning.example}</em>}
+    <div className="meanings">
+      <h4>{meaning.partOfSpeech}</h4>
+      <p>
+        <strong>Definition:</strong>
+        {` `}
+        {meaning.definition}
+      </p>
+      {meaning.example && (
+        <em>
+          <strong>Example:</strong> {meaning.example}
+        </em>
+      )}
+      {meaning.synonyms && meaning.synonyms.length > 0 && (
+        <p>
+          <strong>Synonyms:</strong> {meaning.synonyms.join(", ")}
+        </p>
+      )}
     </div>
   );
 }
