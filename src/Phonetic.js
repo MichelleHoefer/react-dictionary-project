15a471c152b02b996c25b4d80ad934d8b321114f
Michelle Hoefer <michellehoefer7@gmail.com>
2025-06-30 11:30:59 +0100

    added phonetics to the app

---
+++ src/Phonetic.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Phonetic.css";
+
+export default function Phonetic(props) {
+  function handleSpeak() {
+    const utterance = new SpeechSynthesisUtterance(props.word);
+    utterance.lang = "en-GB";
+    utterance.rate = 0.9;
+    window.speechSynthesis.speak(utterance);
+  }
+
+  return (
+    <div className="phonetic">
+      <span className="phonetic-text">/{props.phonetic}/</span>
+      <button onClick={handleSpeak} className="speak-button" title="Listen">
+        🔊
+      </button>
+    </div>
+  );
+}
--- src/Results.js
+++ src/Results.js
@@ -1,12 +1,23 @@
 import React from "react";
 import "./Results.css";
 import Meaning from "./Meaning";
+import Phonetic from "./Phonetic";
 
 export default function Results(props) {
   if (props.results) {
     return (
       <div className="results">
         <h2>{props.results.word}</h2>
+        {props.results.phonetic && (
+          <div className="phonetic">
+            <Phonetic
+              phonetic={props.results.phonetic}
+              word={props.results.word}
+            />
+          </div>
+        )}
+        <br />
+
         {props.results.meanings.map((meaning, index) => (
           <Meaning key={index} meaning={meaning} />
         ))}
